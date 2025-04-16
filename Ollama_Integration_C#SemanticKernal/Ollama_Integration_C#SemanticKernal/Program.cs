2f6d43f6caabb2a37dd70e00f6ffaf648d8f56d9
zakariabijoy <zakaria.bijoy@live.com>
2025-04-17 03:44:34 +0600

    Add .NET project for Ollama Integration with chat service
    Created/modified project and solution files for a new .NET
    application targeting .NET 8.0. Implemented a console app
    in `Program.cs` that utilizes Microsoft Semantic Kernel
    for chat interactions, including necessary package references.

---
+++ Ollama_Integration_C#SemanticKernal/Ollama_Integration_C#SemanticKernal/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder();
+builder.AddOllamaChatCompletion("llama3.2:latest", new Uri("http://localhost:11434"));
+
+var kernel = builder.Build();
+var chatService  = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory();
+history.AddSystemMessage("You are a helpful personal assistant of mine. Your name is katherine and my name is Zakaria");
+
+while (true)
+{
+    Console.Write("\nZakaria: ");
+    var userMessage = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(userMessage)) break;
+    history.AddUserMessage(userMessage);
+
+    var response = await chatService.GetChatMessageContentAsync(history);
+    Console.WriteLine($"\nkatherine Assistant: {response.Content}");
+    history.AddMessage(response.Role, response.Content ?? string.Empty);
+}
+
